3d2a43fde8871a236450af0d647fef49854dc1ea
Jarod <schuppjarod@gmail.com>
2023-03-08 12:39:53 -0500

    Logout Button MainController

--- src/main/java/controllers/appointmentscheduler/MainController.java
+++ src/main/java/controllers/appointmentscheduler/MainController.java
@@ -1,14 +1,39 @@
 package controllers.appointmentscheduler;
 
+import jarod.appointmentscheduler.MainApplication;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
+    public Button logoutButton;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void logoutButtonClick(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("Are you sure you want to logout?");
+        alert.setContentText("Click OK to logout.");
+        alert.showAndWait().ifPresent(rs -> {
+            if (rs == ButtonType.OK) {
+                System.out.println("Logging Out");
+                MainApplication loginScene = new MainApplication();
+                try {
+                    loginScene.start(new Stage());
+                    Stage stage = (Stage) logoutButton.getScene().getWindow();
+                    stage.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 }
