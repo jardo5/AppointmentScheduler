4e7f20ffc46b8c97c41b7a4fec028e4d3d0ffa97
Jardo <schuppjarod@gmail.com>
2023-03-11 17:18:07 -0500

    Login logs in login_activity.

--- src/main/java/controllers/appointmentscheduler/LoginController.java
+++ src/main/java/controllers/appointmentscheduler/LoginController.java
@@ -1,12 +1,22 @@
 package controllers.appointmentscheduler;
 
+import database.appointmentscheduler.AppSQL;
 import database.appointmentscheduler.Query;
 import database.appointmentscheduler.UserSQL;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 import java.net.URL;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.TimeZone;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -15,6 +25,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import models.appointmentscheduler.Appointments;
 import models.appointmentscheduler.Users;
 
 public class LoginController implements Initializable {
@@ -33,7 +44,7 @@ public class LoginController implements Initializable {
         passwordField.getText()
       );
 
-      if (loggedUser != null) { // check if a user was successfully logged in
+      if (loggedUser != null) { // If user is found
         Stage stage = (Stage) ((Button) actionEvent.getSource()).getScene()
           .getWindow();
         FXMLLoader loader = new FXMLLoader(
@@ -43,8 +54,24 @@ public class LoginController implements Initializable {
         stage.setTitle("Appointment Scheduler");
         stage.setScene(scene);
         stage.show();
+
+        //Alert if an appointment is within 15 minutes
+        scheduleAlert();
+
+        //login_activity success attempt
+        System.out.println("Successful username or password add to login_activity.txt");
+        PrintWriter pw = new PrintWriter( new FileOutputStream(("src/main/java/login_activity.txt"),true));
+        pw.append("Success: User: " + loggedUser.getUser_Name() + " logged in at " + LocalDateTime.now() + ".\n");
+        pw.close();
+
+
       } else {
-        // show an error message
+        System.out.println("Invalid username or password add to login_activity.txt");
+        //login_activity failed attempt
+        PrintWriter pw = new PrintWriter( new FileOutputStream(("src/main/java/login_activity.txt"),true));
+        pw.append("Failed: User: " + usernameField.getText() + " attempted to log in at " + LocalDateTime.now() + ".\n");
+        pw.close();
+
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Login Error");
         alert.setHeaderText("Invalid username or password");
@@ -70,6 +97,32 @@ public class LoginController implements Initializable {
           stage.close();
         }
       });
+  }
+
+  public static void scheduleAlert() throws SQLException {
+    ObservableList<Appointments> allAppointments = AppSQL.getAllAppointments();
+    ObservableList<Appointments> fifteenMinAppointments = FXCollections.observableArrayList();
+    // Get all appointments within 15 minutes
+    //alert if appointment is within 15 minutes or not
+    for (Appointments appointment : allAppointments) {
+      if (appointment.getStart().isBefore(LocalDateTime.now().plusMinutes(15)) && appointment.getStart().isAfter(LocalDateTime.now())) {
+        fifteenMinAppointments.add(appointment);
+      }
+    }
+    if (fifteenMinAppointments.size() > 0) {
+      Alert alert = new Alert(Alert.AlertType.INFORMATION);
+      alert.setTitle("Appointment Alert");
+      alert.setHeaderText("There is an appintment within 15 minutes");
+      alert.setContentText("There are " + fifteenMinAppointments.size() + " appointments within 15 minutes");
+      alert.showAndWait();
+    } else {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Appointment Alert");
+        alert.setHeaderText("There are no appointments within 15 minutes");
+        alert.setContentText("There are " + fifteenMinAppointments.size() + " appointments within 15 minutes");
+        alert.showAndWait();
+    }
+
   }
 
   private ResourceBundle Language;
