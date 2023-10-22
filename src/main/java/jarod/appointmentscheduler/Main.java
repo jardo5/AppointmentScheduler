8752931d2d3ee44a0283ffdb1cff960124b8449e
Jardo <schuppjarod@gmail.com>
2023-10-21 20:14:29 -0400

    Maven Clean

---
+++ src/main/java/jarod/appointmentscheduler/Main.java
@@ -0,0 +1,9 @@
+package jarod.appointmentscheduler;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        MainApplication.main(args);
+    }
+}
